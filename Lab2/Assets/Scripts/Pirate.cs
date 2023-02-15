e77e5269f4c36b918874e5250fb92fada008007d
keyszl <89661125+keyszl@users.noreply.github.com>
2023-02-14 20:24:51 -0600

    adding dialog to pirate

--- Lab2/Assets/Scripts/Pirate.cs
+++ Lab2/Assets/Scripts/Pirate.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Pirate : MonoBehaviour
 {
     private AudioSource audio1;
+    public string text;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +19,17 @@ public class Pirate : MonoBehaviour
         
     }
 
-    void OnTriggerEnter2D(Collider2D col) {
+    public void OnTriggerEnter2D(Collider2D col) {
         print("Collision");
         if (col.gameObject.tag == "Player") {
             audio1.Play();
+            GameManager.Instance.DialogShow(text);
+        }
+    }
+    
+    public void OnTriggerExit2D(Collider2D collider2D) {
+        if (collider2D.gameObject.CompareTag("Player")) {
+            GameManager.Instance.DialogHide();
         }
     }
 
