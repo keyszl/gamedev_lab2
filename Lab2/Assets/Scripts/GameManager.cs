493dbb4499f412598567c40f4ad34aa457cd5ee5
keyszl <89661125+keyszl@users.noreply.github.com>
2023-02-14 20:26:30 -0600

    updating game manage for dialogue

--- Lab2/Assets/Scripts/GameManager.cs
+++ Lab2/Assets/Scripts/GameManager.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //public GameObject dialogbox;
-    //public TextMeshProUGUI dialogText;
+    public static GameManager Instance {get; private set;}
+
+    public GameObject dialogBox;
+    public TextMeshProUGUI dialogText;
+    
+    
+    public void DialogShow(string text) {
+        dialogBox.SetActive(true);
+        StopAllCoroutines();
+        StartCoroutine(TypeText(text));
+    }
+    
+    public void DialogHide() {
+        dialogBox.SetActive(false);
+    }
+
+    IEnumerator TypeText(string text) {
+        dialogText.text = "";
+        foreach (char c in text.ToCharArray()) {
+            dialogText.text += c;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+    
+    
     // Start is called before the first frame update
     void Start()
     {
